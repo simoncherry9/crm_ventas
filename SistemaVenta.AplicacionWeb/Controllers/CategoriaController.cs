f160aebaf2429a84aba7ac6ccc29f3aad7bca6a3
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-18 08:28:09 -0300

    Creacion de metodos para categoria y comenzando con la implementacion

--- SistemaVenta.AplicacionWeb/Controllers/CategoriaController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/CategoriaController.cs
@@ -1,12 +1,93 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.AplicacionWeb.Utilidades.Response;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
+
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
     public class CategoriaController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly ICategoriaService _categoriaServicio;
+
+        public CategoriaController(IMapper mapper, ICategoriaService categoriaServicio)
+        {
+            _mapper = mapper;
+            _categoriaServicio = categoriaServicio;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<VMCategoria> vmCategoriaLista = _mapper.Map<List<VMCategoria>> (await _categoriaServicio.Lista());
+            return StatusCode (StatusCodes.Status200OK, new {data = vmCategoriaLista});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromBody]VMCategoria modelo)
+        {
+            GenericResponse<VMCategoria>genericResponse = new GenericResponse<VMCategoria> ();
+
+            try
+            {
+                Categoria categoria_creada = await _categoriaServicio.Crear(_mapper.Map<Categoria>(modelo));
+                modelo = _mapper.Map<VMCategoria>(categoria_creada);
+
+                genericResponse.Estado = true;
+                genericResponse.Objeto = modelo;
+            }
+            catch (Exception ex)
+            {
+                genericResponse.Estado = false;
+                genericResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, genericResponse);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Editar([FromBody] VMCategoria modelo)
+        {
+            GenericResponse<VMCategoria> genericResponse = new GenericResponse<VMCategoria>();
+
+            try
+            {
+                Categoria categoria_editada = await _categoriaServicio.Editar(_mapper.Map<Categoria>(modelo));
+                modelo = _mapper.Map<VMCategoria>(categoria_editada);
+
+                genericResponse.Estado = true;
+                genericResponse.Objeto = modelo;
+            }
+            catch (Exception ex)
+            {
+                genericResponse.Estado = false;
+                genericResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, genericResponse);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Eliminar(int idCategoria)
+        {
+            GenericResponse<string> genericResponse = new GenericResponse<string>();
+
+            try
+            {
+                genericResponse.Estado = await _categoriaServicio.Eliminar(idCategoria);
+            }
+            catch (Exception ex)
+            {
+                genericResponse.Estado = false;
+                genericResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, genericResponse);
         }
     }
 }
