a7044129d680cc8969cb2ead11de5f5f0aadffbb
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-18 08:06:47 -0300

    Agregando inyeccion de dependencia

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -34,8 +34,8 @@ namespace SistemaVenta.IOC
             services.AddScoped<IRolService, RolService>();
 
             services.AddScoped<IUsuarioService, UsuarioService>();
-
             services.AddScoped<INegocioService, NegocioService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
 
         }
     }
